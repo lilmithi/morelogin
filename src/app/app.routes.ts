d7da74fe528bd7955b6b91d97644ed140bd47b8b
lilmithi <gathitumithi9000@gmail.com>
2024-01-05 17:55:13 +0300

    Update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.routes').then((m) => m.authRoutes),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/shared/shared.routes').then((m) => m.sharedRoutes),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
